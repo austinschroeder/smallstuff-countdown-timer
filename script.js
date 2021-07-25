2a2ac11303f7b7738ac3e03ac87ea41ca2085ac5
austinschroeder <austinsoundy@gmail.com>
2021-07-25 13:16:45 -0700

    Connected timer JS to html elements

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-const daysEl = document.getElementById('days');
-const hoursEl = document.getElementById('hours');
-const minutesEl = document.getElementById('minutes');
-const secondsEl = document.getElementById('seconds');
 
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
 
-const newYears = '1 Jan 2022';
+const newYears = "1 Jan 2022";
 
 function countdown() {
-  const newYearsDate = new Date(newYears);
-  const currentDate = new Date();
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-  const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-  const days = Math.floor(totalSeconds / 3600 / 24)
-  const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
-  const seconds = Math.floor(totalSeconds % 60);
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days, hours, minutes, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
-//intial call
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
 countdown();
 
 setInterval(countdown, 1000);
